73f822455cfb554b9bd47445b580f06fa8ce24d7
tmklb <143955800+tmklb@users.noreply.github.com>
2023-11-08 18:29:11 -0600

    Fixed navbar to be responsive on mobile

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -7,21 +7,24 @@ const rubik = Rubik({
   subsets: ['latin']
 })
 
+import {RxHamburgerMenu} from 'react-icons/rx';
+
 export default function NavBar(props) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [eventsActive, setEventsActive] = useState(false);
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <a href="/" className={styles.logo}>
-          <img src="/logo.svg" className={styles.logo} alt="TMKLB Logo"/>
+          <img src="/logo.svg" alt="TMKLB Logo"/>
         </a>
-        <ul className={`${styles.nav_links} ${rubik.className}`}>
-          <li className={`${styles.nav_item} ${props.about ? styles.active : ''}`}><a href="/about">About</a></li>
-          <li className={styles.nav_item}>
-            <div className={`${styles.dropdown} ${menuOpen ? styles.active : ''}`}>
+        <div className={`${styles.nav_links} ${rubik.className} ${menuOpen ? styles.open : ''}`}>
+          <div className={`${styles.nav_item} ${props.about ? styles.active : ''}`}><a href="/about">About</a></div>
+          <div className={styles.nav_item}>
+            <div className={`${styles.dropdown} ${eventsActive ? styles.active : ''}`}>
               <div className={styles.dropdown_btn} onClick={() => {
-                setMenuOpen(!menuOpen);
+                setEventsActive(!eventsActive);
               }}>
                 Events
               </div>
@@ -31,11 +34,16 @@ export default function NavBar(props) {
                 <a className={`${props.thanks ? styles.active : ''}`} href="/thanks">🦃&nbsp;&nbsp;Thanks</a>
               </div>
             </div>
-          </li>
-          <li className={styles.nav_item}><div className={styles.divider}></div></li>
-          <li className={styles.nav_item}><span>Sign in</span></li>
-          <li className={styles.nav_item}><div className={styles.button}>Sign up</div></li>
-        </ul>
+          </div>
+          <div className={`${styles.nav_item} ${styles.div}`}><div className={styles.divider}></div></div>
+          <div className={styles.nav_item}><span>Sign in</span></div>
+          <div className={`${styles.nav_item} ${styles.btn}`}><div className={styles.button}>Sign up</div></div>
+        </div>
+        <div className={styles.nav_menu} onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}>
+          <RxHamburgerMenu />
+        </div>
       </div>
     </nav>
   )
